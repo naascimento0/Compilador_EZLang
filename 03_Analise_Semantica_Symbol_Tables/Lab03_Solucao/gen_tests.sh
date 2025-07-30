256786009594ade78cf762fc5655f81cf2a31ae5
naascimento0 <gabrieloliveiran@hotmail.com>
2025-07-30 09:40:40 -0300

    Analise Semantica Symbol Tables finalizada

--- 03_Analise_Semantica_Symbol_Tables/Lab03_Solucao/gen_tests.sh
+++ 03_Analise_Semantica_Symbol_Tables/Lab03_Solucao/gen_tests.sh
@@ -24,21 +24,25 @@ CURRENT_DIR=$(pwd)
 ROOT=$(dirname "$CURRENT_DIR")  # Vai para 03_Analise_Semantica_Symbol_Tables
 PROJECT_ROOT=$(dirname "$ROOT")  # Vai para Compilador_EZLang
 
-ANTLR_PATH="$CURRENT_DIR/tools/antlr-4.13.2-complete.jar"
+ANTLR_PATH="$PROJECT_ROOT/tools/antlr-4.13.2-complete.jar"
 CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
 
 GRAMMAR_PREFIX=EZ
 GEN_PATH=parser
 BIN_PATH=bin
 
-IN="$ROOT/Lab03_Input"
+IN="$PROJECT_ROOT/Inputs_Labs"
 OUT="$ROOT/Lab03_Output"
 EXPECTED="$ROOT/Lab03_Output_Expected"
 
 # Verificar se o diretório de saída existe
 mkdir -p "$OUT"
 
-cd "$GEN_PATH"
+if [[ ! -d "$GEN_PATH" ]]; then
+    echo "Please run make first to generate the parser files."
+    exit 1
+fi
+
 for infile in "$IN"/*.ezl; do
     if [[ -f "$infile" ]]; then
         base=$(basename "$infile")
